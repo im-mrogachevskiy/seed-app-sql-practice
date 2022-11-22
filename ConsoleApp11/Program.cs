cde977e43001f34dc733b60995f2262ab3516617
Martin Rogachevskiy <58072362+im-mrogachevskiy@users.noreply.github.com>
2022-11-22 19:13:20 +0300

    for linq practice

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using System.Linq;
+using ConsoleApp11.Models;
 
 namespace ConsoleApp11
 {
     class Program
     {
+        private static Address[] _addresses;
+        private static Person[] _persons;
+        private static University[] _universities;
+        private static StudySubject[] _studySubjects;
+        private static PersonStudySubject[] _personStudySubjects;
+
+        static Program()
+        {
+            SeedData();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // your code 
+        }
+
+        static void SeedData()
+        {
+            _addresses = Address.Generate(100).ToArray();
+
+            _persons = Person.Generate(350, _addresses.Select(x => x.Id).ToArray()).ToArray();
+            _universities = University.Generate(5, _addresses.Select(x => x.Id).ToArray()).ToArray();
+
+            _studySubjects = StudySubject.Generate(25, _universities.Select(x => x.Id).ToArray(),
+                _persons.Select(x => x.Id).ToArray()).ToArray();
+
+            _personStudySubjects = PersonStudySubject.Generate(200, _studySubjects.Select(x => x.Id).ToArray(),
+                    _persons.Select(x => x.Id).ToArray()).Select(x => (x.StudentId, x.SubjectId)).ToHashSet()
+                .Select(x => new PersonStudySubject() { StudentId = x.StudentId, SubjectId = x.SubjectId }).ToArray();
         }
     }
 }
